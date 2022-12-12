4804780c2a9493f349e42e790b99f8c258bec18a
ARINA DUNAI <dunaj03@mail.ru>
2022-12-12 21:00:59 +0300

    6

--- №6/code.php
+++ №6/code.php
@@ -2,36 +2,40 @@
 // функция подключения
 function Connect_dataBase()
 {
-  $serverName = "localhost";
-  $db_user = "root";
-  $db_pass = "";
+  $serverName = "localhost";// сервер
+  $db_user = "root";//Имя пользователя
+  $db_pass = "";//Пароль пользователя
   $db_name = "my_bd"; // название базы данных
 
-  $dataBase = new mysqli($serverName, $db_user, $db_pass, $db_name);
+  $dataBase = new mysqli($serverName, $db_user, $db_pass, $db_name);//выполнение подключения
   return $dataBase;
 }
 
 // нажатие на кнопку Авториззоваться
 if (isset($_GET['autorisation'])) {
+  //определяет, объявлена ли переменная и отличается ли она от null
+  // Данные храняться в массиве $_GET
   $login = $_GET['login'];
   $password = $_GET['password'];
   if ($login != null && $password != null) {
+    //проверка на ошибки
     $dataBase = Connect_dataBase();
-
+    // соединение с бд
     $request = "SELECT login, password FROM users WHERE login = '$login' AND password = '$password'";
-
+    // добавление логина авторизованного пользователя, и его пароль добаляется в таблицу users 
     $result = mysqli_fetch_assoc($dataBase->query($request));
-
+    // извлекает одну строку данных из результирующего набора и возвращает ее в виде ассоциативного массива. 
     if($result != null){
       header("Location: site.php");
     }
+    //если нет повторяющегося значения, то происходит переход на сайт
     else {
       header("Location: index.php");
     }
-
-    $dataBase->close();
+    // иначе происходит возвращение на первоначальную страницу
+    $dataBase->close(); // закрытие бд
   } else {
-    readfile('index.php');
+    readfile('index.php'); // иначе вывод файла индекс.php
   }
 }
 
@@ -43,6 +47,7 @@ if (isset($_GET['registration'])) {
   if ($login != null && $password != null && $repeatPassword != null) {
     if ($password == $repeatPassword) {
       $dataBase = Connect_dataBase();
+      
 
       // написать проверку на существование такого аккаунта с таким же логином с использованием SELECT
       // (как использовать SELECT смотрите в нажатии на кнопку Авторизоваться)
